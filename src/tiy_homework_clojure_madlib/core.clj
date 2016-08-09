8324aa75344634cb3574f68e67c071c425ffa29b
dave hochstrasser <davehochstrasser@daves-MacBook-Pro.local>
2016-08-09 16:43:19 -0400

    first commit

---
+++ src/tiy_homework_clojure_madlib/core.clj
@@ -0,0 +1,32 @@
+(ns tiy-homework-clojure-madlib.core)
+
+(defn madlib[]
+            (let[ relative "Aunt Sally "
+                 relative2 "Uncle "
+                 adjective "large "
+                 adjective2 "melodic "
+                 adjective3 "similar "
+                 adjective4 "understood "
+                 adjective5 "aching "
+                 person-in-room "Winston "
+                 person-in-room2 "Fred "
+                 verb-ed "bribed "
+                 body-part "back "
+                 verb-ing "wheedling "
+                 noun-plural "terms "
+                 verb "smell "
+                 verb2 "adjust"
+                 noun "joke "
+                 adverb "slippery "
+                 adverb2 "cloyingly"
+                 ]
+              (println "Dear " relative "," "I am having a(n)"
+                       adjective "time at camp. The counselour is" adjective2
+                       "and the food is " adjective3". I met "
+                       person-in-room "and we became "adjective4 "friends.
+                       Unfortunately, "person-in-room2 "is "adverb "and I "
+                       verb-ed "my "body-part "so we couldn`t go "verb-ing "like everybody else.
+                       I need more "noun-plural "and a "noun "sharpener, so please "adverb2 verb
+                       "more when you "adjective5 "back.
+                       Your "relative2 ", "person-in-room)))
+(madlib)
